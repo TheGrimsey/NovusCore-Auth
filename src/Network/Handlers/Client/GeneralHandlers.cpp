b57d8b2696172b2b92b2b3f7a3df7cf59e3820cc
TheGrimsey <TheGrimsey@users.noreply.github.com>
2022-04-26 17:58:42 +0200

    Minor realm related fixes.

--- src/Network/Handlers/Client/GeneralHandlers.cpp
+++ src/Network/Handlers/Client/GeneralHandlers.cpp
@@ -14,7 +14,7 @@ namespace Client
     void GeneralHandlers::Setup(NetPacketHandler* netPacketHandler)
     {
         netPacketHandler->SetMessageHandler(Opcode::CMSG_REQUEST_REALMLIST, { ConnectionStatus::CONNECTED, 0, GeneralHandlers::HandleRequestRealmlist });
-        netPacketHandler->SetMessageHandler(Opcode::CMSG_SELECT_REALM, { ConnectionStatus::CONNECTED, 1, GeneralHandlers::HandleSelectRealm });
+        netPacketHandler->SetMessageHandler(Opcode::CMSG_SELECT_REALM, { ConnectionStatus::CONNECTED, sizeof(u16), GeneralHandlers::HandleSelectRealm});
     }
 
     bool GeneralHandlers::HandleRequestRealmlist(std::shared_ptr<NetClient> netClient, std::shared_ptr<NetPacket> packet)
@@ -32,13 +32,13 @@ namespace Client
         entt::registry* registry = ServiceLocator::GetRegistry();
         RealmlistCacheSingleton& realmlistCacheSingleton = registry->ctx<RealmlistCacheSingleton>();
 
-        u8 realmIndex = 0;
-        if (!packet->payload->GetU8(realmIndex))
+        u16 realmID = 0;
+        if (!packet->payload->GetU16(realmID))
             return false;
 
         std::shared_ptr<Bytebuffer> buffer = Bytebuffer::Borrow<128>();
 
-        auto itr = realmlistCacheSingleton.realmlistMap.find(realmIndex);
+        auto itr = realmlistCacheSingleton.realmlistMap.find(realmID);
         if (itr == realmlistCacheSingleton.realmlistMap.end())
         {
             // Realm does not exist, send back "SMSG_SEND_ADDRESS" to tell the client theres no available server
@@ -50,7 +50,7 @@ namespace Client
         }
         else
         {
-            if (!PacketUtils::Write_MSG_REQUEST_ADDRESS(buffer, AddressType::REALM, netClient->GetEntity(), &realmIndex, 1))
+            if (!PacketUtils::Write_MSG_REQUEST_ADDRESS(buffer, AddressType::REALM, netClient->GetEntity(), reinterpret_cast<u8*>(&realmID), sizeof(u16)))
                 return false;
 
             auto& connection = registry->ctx<ConnectionSingleton>();
